3e36f8a6d5b0c37b24ef08ba31affc298029a00b
martinlgalva <martinlgalvan@yahoo.com>
2023-06-02 16:04:19 -0300

    d

--- api/index.js
+++ api/index.js
@@ -3,9 +3,6 @@ import cors from 'cors'
 import WeekApiRoute from './routes/week.api.routes.js'
 import UsersApiRoute from './routes/users.api.routes.js'
 import ExercisesApiRoute from './routes/exercises.api.routes.js'
-import NotesApiRoute from './routes/notes.api.routes.js'
-
-
 
 
 const app = express()
@@ -18,6 +15,5 @@ app.use(express.json())
 app.use('/', UsersApiRoute)
 app.use('/', WeekApiRoute)
 app.use('/', ExercisesApiRoute)
-app.use('/', NotesApiRoute)
 
 export default app
--- express.js
+++ express.js
@@ -3,8 +3,6 @@ import cors from 'cors'
 import WeekApiRoute from './api/routes/week.api.routes.js'
 import UsersApiRoute from './api/routes/users.api.routes.js'
 import ExercisesApiRoute from './api/routes/exercises.api.routes.js'
-import NotesApiRoute from './api/routes/notes.api.routes.js'
-
 
 
 const app = express()
@@ -16,7 +14,6 @@ app.use(express.static('public'))
 app.use('/', UsersApiRoute)
 app.use('/', WeekApiRoute)
 app.use('/', ExercisesApiRoute)
-app.use('/', NotesApiRoute)
 
 
 app.listen(2022, function () {
