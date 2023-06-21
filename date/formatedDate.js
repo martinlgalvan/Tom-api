be88c4791e6fbbeb0d6af9fb08664c9060b53193
martinlgalva <martinlgalvan@yahoo.com>
2023-06-21 01:28:18 -0300

    formatedAMPM

--- date/formatedDate.js
+++ date/formatedDate.js
@@ -6,30 +6,26 @@ export function getDate() {
   var año = fecha.getUTCFullYear();
   
   // Asegurarse de que el día y el mes tengan dos dígitos
-  dia = dia < 10 ? '0' + dia : dia;
-  mes = mes < 10 ? '0' + mes : mes;
+  dia = dia.toString().padStart(2, '0');
+  mes = mes.toString().padStart(2, '0');
   
   var hora = fecha.getUTCHours() - 3; // Restar 3 horas para obtener la hora en GMT-3
   
   // Asegurarse de que la hora esté dentro del rango de 0 a 23
   hora = hora < 0 ? 24 + hora : hora;
   
-  var minutos = fecha.getUTCMinutes();
-  
-  // Asegurarse de que los minutos tengan dos dígitos
-  minutos = minutos < 10 ? '0' + minutos : minutos;
-  
-  var segundos = fecha.getUTCSeconds();
-  
-  // Asegurarse de que los segundos tengan dos dígitos
-  segundos = segundos < 10 ? '0' + segundos : segundos;
+  var minutos = fecha.getUTCMinutes().toString().padStart(2, '0');
+  var segundos = fecha.getUTCSeconds().toString().padStart(2, '0');
   
   var horaFormateada = hora + ':' + minutos + ':' + segundos;
+
+  var indicacionHorario = hora < 12 ? 'AM' : 'PM';
   
   var fechaFormateada = {
     fecha: dia + '/' + mes + '/' + año,
-    hora: horaFormateada
+    hora: horaFormateada + ' ' + indicacionHorario
   };
   
   return fechaFormateada;
 }
+
